853703197ad334145295f3ac3d1ce0bf331f5f78
nikogniko <gorbach.nika.m@gmail.com>
2026-09-09 16:38:07 +0300

    add modal window

---
+++ udemy-js/modal/script.js
@@ -0,0 +1,43 @@
+//  JavaScript in browser: DOM and Events
+//  Project 2. Modal window
+
+// Plan:
+// - Modal window
+// - Working with classes
+// - Handling Keyboard events
+
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// keydown event is a global event that is fired whenever a key is pressed down on the keyboard
+//
+// the event object is passed to the callback function automatically
+//
+document.addEventListener('keydown', function (event) {
+  console.log(event.key);
+
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
